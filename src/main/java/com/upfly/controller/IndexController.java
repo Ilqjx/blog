31b9f9dce0dd279ec0a39a13db52a88110ec1c12
Ilqjx <1695151719@qq.com>
2020-02-18 10:33:49 +0800

    页面处理

--- src/main/java/com/upfly/controller/IndexController.java
+++ src/main/java/com/upfly/controller/IndexController.java
@@ -2,13 +2,12 @@ package com.upfly.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name) { // @PathVariable接受url中的数据
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 
